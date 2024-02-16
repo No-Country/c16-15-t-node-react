6c055bf0e35b768f49a84e1aa970d04afc1c6d14
Erick Henriquez <erickdavid7@hotmail.com>
2024-02-15 19:33:14 -0500

    improved readability and error handling

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,13 +1,15 @@
 import mongoose from 'mongoose';
 import { config } from 'dotenv';
-config({ path: process.ENV });
+config();
 
 const connectDB = async function(){
   try{
     await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to database");
     return mongoose.connection;
   } catch (error){
-    console.log(error);
+    console.error(error.message);
+    throw error;
   }
 }
 
