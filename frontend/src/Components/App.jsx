7ca5af92b98f487bf647f478ed4dcd745ee84dda
CassieGz <110521493+CassieGz@users.noreply.github.com>
2024-02-20 04:48:40 -0300

    creating style folders for each component.Adding styles to App and index. Logo addition.

--- frontend/src/Components/App.jsx
+++ frontend/src/Components/App.jsx
@@ -1,12 +1,13 @@
 import Header from './Header';
+import Logo from './Logo';
 import ProyectsGrid from './ProyectsGrid';
-
 
 
 function App() {
 
   return (
     <>
+      <Logo/>
       <Header/>      
       <main>
         <h1>Bienvenido/a a EcoFinder!</h1>
--- frontend/src/Components/Logo.jsx
+++ frontend/src/Components/Logo.jsx
@@ -1,8 +1,13 @@
+import React from "react";
+import "../styles/Logo.css";
+import logo_eco from "../assets/logo_eco"
+
+
 function Logo() {
     return (
-        <div>
-            <img
-                src='/'
+        <div className="logo-container">
+            <img className="logo-imagen"
+                src={logo_eco}
                 alt='/Logo'
             />
         </div>
