03da9ba503d25585ae4f7e929f053803e9547f56
Erick Henriquez <erickdavid7@hotmail.com>
2024-02-29 12:25:11 -0500

    changed language to english

--- backend/src/controllers/apis/api_arg.controller.js
+++ backend/src/controllers/apis/api_arg.controller.js
@@ -6,10 +6,11 @@ export const getAll = async (req, res) => {
     const database = await connectDB();
     const collection = database.collection('places');
     const placesCollection = await collection.find({}).toArray();
-    return res.status(200).send({ places: placesCollection, status: 'OK' });
+    const lengthCollection = placesCollection.length;
+    return res.status(200).send({ places: placesCollection, length: lengthCollection, status: 'OK' });
   } catch (error) {
-    console.error('Error al obtener datos de la base de datos:', error.message);
-    return res.status(500).send({ error: 'Error al obtener datos de la base de datos' });
+    console.error('Error obtaining data from database: ' + error.message);
+    return res.status(500).send({ error: 'Error obtaining data from database:' });
   }
 };
 
@@ -23,9 +24,9 @@ export const getOrder = async(req, res) => {
     if (placeDB) return res.status(200).send({order: placeDB, status: 'OK'});
     else return res.status(400).send({error: 'Id not found'})
   }catch (error){
-    console.error('Error al obtener datos de la base de datos:', error.message);
-    return res.status(500).send({ error: 'Error al obtener datos de la base de datos' });
+    console.error('Error obtaining data from database: ' + error.message);
+    return res.status(500).send({ error: 'Error obtaining data from database:' });
   }
 
-}
+};
 
